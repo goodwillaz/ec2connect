a3d70e46d984042a9661113835c68dd8f85028fe
Matt Zuba <matt.zuba@goodwillaz.org>
2025-06-09 14:21:32 -0700

    fix build num gen [CI SKIP]

--- scripts/build-number.sh
+++ scripts/build-number.sh
@@ -6,7 +6,8 @@ set -x
 apk add --no-progress --quiet git
 
 if [ -z "${DRONE_TAG}" ]; then
-  BUILD_COUNT=$(drone build ls --server https://${DRONE_SYSTEM_HOST} --branch ${DRONE_BRANCH} --limit 200 --format "{{ .Number }}" ${DRONE_REPO} | wc -l)
+  export DRONE_SERVER=https://${DRONE_SYSTEM_HOST}
+  BUILD_COUNT=$(drone build ls --branch ${DRONE_BRANCH} --limit 200 --format "{{ .Number }}" ${DRONE_REPO} | wc -l)
   DRONE_TAG=${DRONE_BRANCH##*/v}rc$((++BUILD_COUNT))
 fi
 
